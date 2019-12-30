04135bf1dc24087b3f1b9769663e32b335e205f3
Rob Saunders <rob@wearebrandnew.com>
2019-12-30 18:10:15 +1100

    returns basic mock data

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 var JsonRpcWs = require('json-rpc-ws/browser');
+var host = 'ws://localhost:8080';
 
 window.onload = () => {
-    document.body.textContent = "typescript loaded";
-
+    console.log("window loaded");
     var client = JsonRpcWs.createClient();
 
-    client.connect('ws://localhost:8080', function connected () {
+    client.connect(host, function connected () {
+        console.log("connected to ", host);
         client.send('list-models', { limit: 0 }, function mirrorReply (error, reply) {
+            console.log('reply -> ', reply);
             document.body.textContent = reply;
-            // console.log('reply -> ', reply);
         });
     });
 };
