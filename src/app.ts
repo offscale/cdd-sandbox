ecee4fda6a607468ce22533dffbebcec0513b280
Rob Saunders <rob@wearebrandnew.com>
2020-01-13 11:45:43 +0800

    improved rpc call refactor

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,90 @@
 var JsonRpcWs = require("json-rpc-ws/browser");
 var ace = require("brace");
 require("brace/mode/javascript");
+require("brace/mode/yaml");
 require("brace/theme/monokai");
+
+// implement
+function rpc_call(
+  host: String,
+  method: String,
+  params: any,
+  callback: (response: any) => void
+) {
+  console.log("rpc_call: " + host + method);
+  var client = JsonRpcWs.createClient();
+  client.connect(host, function connected() {
+    client.send(method, params, function mirrorReply(
+      error: any,
+      reply: { [x: string]: any }
+    ) {
+      if (error != null) {
+        console.log(error);
+      } else {
+        callback(reply);
+      }
+    });
+  });
+}
 
 window.onload = () => {
   console.log("window loaded");
 
   // ace editor init
   var editor = ace.edit("javascript-editor");
-  editor.getSession().setMode("ace/mode/javascript");
+  editor.getSession().setMode("ace/mode/yaml");
   editor.setTheme("ace/theme/monokai");
   // end editor init
 
-  function get_service(host: String, method: String) {
-    console.log("get_service called with: " + method);
-    var client = JsonRpcWs.createClient();
-    client.connect(host, function connected() {
-      client.send(method, {}, function mirrorReply(
-        error: any,
-        reply: { [x: string]: any }
-      ) {
-        console.log("reply ->");
-        console.log(reply);
-        console.log("error ->");
-        console.log(error);
-        if (reply != undefined) {
-          let code = reply
-            .map(function(model) {
-              return model["code"];
-            })
-            .join("\n\n");
-          console.log(code);
-          editor.setValue(code);
-        }
+  // openapi default
+  rpc_call("ws://localhost:7777", "default", {}, response => {
+    let code = response["code"];
+    if (code != undefined) {
+      editor.setValue(code);
+      editor.clearSelection();
+      rpc_call("ws://localhost:7777", "parse", { code }, response => {
+        console.log(response);
       });
-    });
-  }
+    } else {
+      console.log(response);
+    }
+  });
 
-  get_service("ws://localhost:8080", "list-models");
-  get_service("ws://localhost:7777", "project-default");
+  // function get_service(host: String, method: String) {
+  //   console.log("get_service called with: " + method);
+  //   var client = JsonRpcWs.createClient();
+  //   client.connect(host, function connected() {
+  //     client.send(method, {}, function mirrorReply(
+  //       error: any,
+  //       reply: { [x: string]: any }
+  //     ) {
+  //       console.log("reply ->");
+  //       console.log(reply);
+  //       console.log("error ->");
+  //       console.log(error);
+  //       if (reply != undefined) {
+  //         let code = reply
+  //           .map(function(model) {
+  //             return model["code"];
+  //           })
+  //           .join("\n\n");
+  //         console.log(code);
+  //         editor.setValue(code);
+  //         editor.clearSelection();
+
+  //         // editor.on('change', function () {});
+
+  //         // refactor this definitely
+  //         rpc_call("ws://localhost:7777", "parse", { code }, response => {
+  //           console.log(response);
+  //         });
+  //       }
+  //     });
+  //   });
+  // }
+
+  // get_service("ws://localhost:8080", "list-models");
+  // get_service("ws://localhost:7777", "default");
 
   // var jstree      = require('./treeview');
   // import * as jsTree from 'treeview';
