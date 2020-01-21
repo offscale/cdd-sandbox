bd494cadae995f347eb1a79b9854201e3cf06113
Rob Saunders <rob@wearebrandnew.com>
2020-01-22 00:55:56 +0800

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const distFolder = path.resolve(__dirname, "public");
+
+module.exports = {
+  entry: ["./src/app.ts", "./src/index.slim"],
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(slm|slim)/,
+        use: [
+          {
+            loader: "file-loader",
+            query: {
+              name: "[name].html"
+            }
+          },
+          { loader: "slm-loader" }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    path: distFolder,
+    filename: "app.js"
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: "assets",
+        to: distFolder
+      }
+    ])
+  ]
+};
