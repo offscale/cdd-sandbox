f0582f734c7a6497f6b12d9c06e80aa17ca1e2f7
nom <deathdisco@protonmail.com>
2020-03-21 21:38:58 +1100

    moving toward monaco

--- src/editor.ts
+++ src/editor.ts
@@ -1,31 +1,76 @@
+import * as monaco from "monaco-editor";
+
 import { State } from "./state";
+import { Domain } from "domain";
+import { DOM } from "./dom";
 
-var ace = require("brace");
-require("brace/mode/javascript");
-require("brace/mode/yaml");
-require("brace/mode/typescript");
-require("brace/mode/rust");
-require("brace/mode/swift");
-require("brace/theme/monokai");
+// var ace = require("brace");
+// require("brace/mode/javascript");
+// require("brace/mode/yaml");
+// require("brace/mode/typescript");
+// require("brace/mode/rust");
+// require("brace/mode/swift");
+// require("brace/theme/monokai");
 
-let editorId = "javascript-editor";
+let editorId = "editor";
+
+// @ts-ignore
+self.MonacoEnvironment = {
+  getWorkerUrl: function(moduleId, label) {
+    if (label === "json") {
+      return "./json.worker.bundle.js";
+    }
+    if (label === "css") {
+      return "./css.worker.bundle.js";
+    }
+    if (label === "html") {
+      return "./html.worker.bundle.js";
+    }
+    if (label === "typescript" || label === "javascript") {
+      return "./ts.worker.bundle.js";
+    }
+    return "./editor.worker.bundle.js";
+  }
+};
+// import * as monaco from "monaco-editor";
+// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+// import * as _ from "lodash";
+// const { JSONPath } = require('jsonpath-plus');
+// const nodejq = require("jq-in-the-browser").default;
+// import monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+
+// monaco.editor.create(document.body, {
+//   value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
+//   language: "typescript"
+// });
+
 
 export module Editor {
   export function init() {
-    var editor = ace.edit(editorId);
-    editor.$blockScrolling = Infinity;
-    editor.setTheme("ace/theme/monokai");
+    // var editor = ace.edit(editorId);
+    // editor.$blockScrolling = Infinity;
+    // editor.setTheme("ace/theme/monokai");
 
-    return editor;
+    // let editor = document.querySelector(editorId);
+    
+    // console.log("DOCUMENT", document.getElementById(editorId));
+
+    // monaco.editor.create(document.body);
+    monaco.editor.create(document.getElementById(editorId), {
+      value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
+      language: "typescript"
+    });
+
+    // return {};
   }
 
   export function update(appState: State.AppState) {
     if (appState.selectedTab) {
       let currentProject = appState.currentProject();
 
-      appState.editor.getSession().setMode(`ace/mode/${currentProject.syntax}`);
-      appState.editor.setValue(currentProject.code);
-      appState.editor.clearSelection();
+      // appState.editor.getSession().setMode(`ace/mode/${currentProject.syntax}`);
+      // appState.editor.setValue(currentProject.code);
+      // appState.editor.clearSelection();
     }
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,36 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const distFolder = path.resolve(__dirname, "public");
+// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
-  entry: ["./src/app.ts", "./src/index.slim"],
-  mode: "production",
-  performance: {
-    hints: false
+  mode: "development",
+  entry: {
+    app: "./src/app.ts", slim: "./src/index.slim",
+    "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
+    "json.worker": "monaco-editor/esm/vs/language/json/json.worker",
+    "css.worker": "monaco-editor/esm/vs/language/css/css.worker",
+    "html.worker": "monaco-editor/esm/vs/language/html/html.worker",
+    "ts.worker": "monaco-editor/esm/vs/language/typescript/ts.worker"
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".css"]
+  },
+  output: {
+    globalObject: "self",
+    filename: "[name].bundle.js",
+    path: distFolder
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        loader: "ts-loader",
-        exclude: path.resolve(__dirname, "node_modules")
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(slm|slim)/,
@@ -26,15 +43,12 @@ module.exports = {
           },
           { loader: "slm-loader" }
         ]
+      },
+      {
+        test: /\.ttf$/,
+        use: ['file-loader']
       }
     ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  output: {
-    path: distFolder,
-    filename: "app.js"
   },
   plugins: [
     new CopyWebpackPlugin([
